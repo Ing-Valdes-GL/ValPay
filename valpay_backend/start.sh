30b043bef9df200e7b576400998583830e053789
ValPay Dev <doungmolagoungvaldes@gmail.com>
2026-05-27 15:07:15 +0100

    fix(railway): make cache commands non-fatal in start.sh

--- valpay_backend/start.sh
+++ valpay_backend/start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-set -e
 
-php artisan config:cache
-php artisan route:cache
+php artisan config:cache || echo "[warn] config:cache failed, continuing..."
+php artisan route:cache  || echo "[warn] route:cache failed, continuing..."
 
 exec php artisan serve --host=0.0.0.0 --port="${PORT:-8000}"
